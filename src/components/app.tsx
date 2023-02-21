758702dca3f996e0006c83627a0a2bf74d986420
Iyzyman <101888183+Iyzyman@users.noreply.github.com>
2023-02-21 21:42:07 +0800

    update navbar

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { getFlats } from '../functions/api'
 import { FlatContext } from './context';
 import '../css/app.css'
@@ -6,46 +6,53 @@ import { Main } from '../pages/main';
 import { LoanRecommendation } from '../pages/LoanRecommendation';
 import { IncomeCalculator } from '../pages/incomeCalculator';
 import { Explore } from '../pages/explore';
+import { AboutUs } from '../pages/aboutus/aboutus';
 import Nav from './navbar/navbar';
 
 
 // The component that displays the current page.
 // Add new pages into the switch statement
-const Body = () => {
-  const [page, setPage] = React.useState("main")
-
-  const switchPage = (newPage: string) => {
-    setPage(newPage)
-  }
-
+type BodyProps = {
+  page: string;
+  switchTo: (newPage: string) => void;
+};
+const Body = ({ page, switchTo }: BodyProps) => {
   let component = null;
   switch (page) {
     case "main":
-      component = <Main switchTo={switchPage} />
-      break
+      component = <Main switchTo={switchTo} />;
+      break;
     case "explore":
-      component = <Explore switchTo={switchPage} />
-      break
+      component = <Explore switchTo={switchTo} />;
+      break;
     case "incomeCalculator":
-      component = <IncomeCalculator switchTo={switchPage} />
-      break
+      component = <IncomeCalculator switchTo={switchTo} />;
+      break;
     case "loanRecommendation":
-      component = <LoanRecommendation switchTo={switchPage} />
-      break
+      component = <LoanRecommendation switchTo={switchTo} />;
+      break;
+    case "aboutus":
+      component = <AboutUs switchTo={switchTo} />;
+      break;
     default:
-      component = null
-      break
+      component = null;
+      break;
   }
   return (
     <div className="app-body">
       {component}
     </div>
-  )
-}
+  );
+};
+
 
 const App = () => {
   const [flats, setFlats] = React.useState([])
+  const [page,setPage] = React.useState("main")
 
+  const switchPage = (newPage: string) => {
+    setPage(newPage)
+  }
   // To call API only once and store in FlatContext
   React.useEffect(() => {
     getFlats()
@@ -55,11 +62,11 @@ const App = () => {
   return (
     <FlatContext.Provider value={flats}>
       <div className="app">
-        <Nav />
-        <Body />
+        <Nav switchTo={switchPage}/>
+        <Body page={page} switchTo={switchPage} />
       </div>
     </FlatContext.Provider>
   );
 }
 
-export { App }
+export { App,Body }
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import './navbar.css';
-
-const Nav = () => {
+import { PageProps } from '../../functions/types';
+import { AboutUs } from '../../pages/aboutus/aboutus';
+const Nav = (props: PageProps) => {
+  
   return (
     <div className="app-nav">
       <img src={require('./teamtwo.png')} alt="RecoFlat logo" />
-      <div className="app-logo">Recoflat</div>
+      <div onClick={() =>props.switchTo("main")}className="app-logo">Recoflat</div>
       <div className="app-actions">
         <div className="app-action">
           <span>How to use?</span>
@@ -14,7 +15,7 @@ const Nav = () => {
           <div className="line"></div>
         </div>
         <div className="app-action">
-        <span>About us</span>
+        <span onClick={() =>props.switchTo("aboutus")}>About us</span>
         <img  src={require('./human.png')} alt="icon" width="30" height="25.33"/>
         <div className="line2"></div>
       </div>
