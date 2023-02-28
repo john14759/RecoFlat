a6cdaa761707a3534cac1dcbd0323026d6ad59ea
Iyzyman <101888183+Iyzyman@users.noreply.github.com>
2023-03-01 02:00:44 +0800

    update

--- src/pages/filterprice.tsx
+++ src/pages/filterprice.tsx
@@ -8,10 +8,14 @@ import React, { useState,useContext } from 'react';
 const FilterPrice = (props: PageProps) => {
     const [min, setMin] = useState<number | undefined>(undefined);
     const [max, setMax] = useState<number | undefined>(undefined);
-    const [min1, setMin1] = useState<number | undefined>(undefined);
-    const [max1, setMax1] = useState<number | undefined>(undefined);
-    const [showResults, setShowResults] = useState(false);
+    const [drop, setDrop] = useState<boolean>(false);
     const flats = useContext(FlatContext);
+    const handleDropdown=()=>{
+      if(drop){
+        setDrop(false);
+      }
+      else{setDrop(true);}
+    }
     const handleMinChange = (event: React.ChangeEvent<HTMLInputElement>) => {
       const inputValue = event.target.value;
       const parsedValue = /^\d+$/.test(inputValue) ? parseInt(inputValue, 10) : undefined;
@@ -45,9 +49,9 @@ const FilterPrice = (props: PageProps) => {
           return averagePrice > minimum && averagePrice < maximum;
         })
         .sort((a, b) => {
-          const avgPriceA = a.totalPrice.dividedBy(a.count).toFormat(0);
-          const avgPriceB = b.totalPrice.dividedBy(b.count).toFormat(0);
-          return avgPriceA.localeCompare(avgPriceB);
+          const avgPriceA = a.totalPrice.dividedBy(a.count).toNumber();
+          const avgPriceB = b.totalPrice.dividedBy(b.count).toNumber();
+          return avgPriceA - avgPriceB;
         });
     // Calculate the average price for each group and render the result
     return filteredGroups.map((group, i) => {
@@ -61,42 +65,43 @@ const FilterPrice = (props: PageProps) => {
         );
       });
     };
-    const handleSearchClick = () => {
-      setShowResults(true);
-      setMin1(min);
-      setMax1(max);
-    }
+    
 
-    const minVal = () => min1 ?? 0;
-    const maxVal = () => max1?? 999999999;
+    const minVal = () => min ?? 0;
+    const maxVal = () => max?? 3000000;
     return (
       <div className='container price'>
         <div className='price-filter'>
             <div className='pricehead'>
               <div className='price-header'>Filter selected: Price Range</div>
                 <label htmlFor="min-input">Price Range</label>
+                <img onClick={handleDropdown}src='img/dropdownlogo.png'></img>
                 <br />
-                  <div className='price-input'>
-                  <input type="text" id="min-input" name="min" value={min ?? ""} onChange={handleMinChange}/>
+                {drop && (
+                <div className='price-input'>
+                  <input type="text" id="min-input" name="min" value={min ?? ""} onChange={handleMinChange} style={{ marginRight: '15px' }}/>
                   to
-                  <input type="text" id="max-input" name="max" value={max ?? ""} onChange={handleMaxChange}/>
-                  <button onClick={handleSearchClick}>Search</button> 
+                  <input type="text" id="max-input" name="max" value={max ?? ""} onChange={handleMaxChange} style={{ marginLeft: '15px' }}/> 
+                  <p>Now displaying the flats in the price range that you have selected</p>
                 </div>
-                <p>Now displaying the flats in the price range that you have selected</p>
+                
+                )}
+                
               </div>
             </div>
-            
-        {showResults ? (
             <div className='priceResults'>
-              Search Results:
+              <div className='search-area'>Search Results:</div>
               <div className='tableheader'>
                 <div>Flat types</div>
                 <div>Location</div>
                 <div>Average sale price</div>
               </div>
+              <div className='price-content'>
               {displayPrice(minVal(), maxVal())}
+              </div>
+              
             </div>
-          ) : null}
+  
         </div>
     )
 }
