9bf5a402da988dc1f34c828f4be82c80bbffc313
john14759 <john14759@gmail.com>
2023-03-12 23:15:10 +0800

    added new dropdown jsx

---
+++ src/components/dropdown.tsx
@@ -0,0 +1,48 @@
+import React, { useState, ChangeEvent } from 'react';
+import '../css/dropdown.css'
+
+function DropdownButton(props: { handleFlatTypeChange: (value: string) => void }) {
+  const [selectedValue, setSelectedValue] = useState('');
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedValue(event.target.value);
+    setIsDropdownOpen(false);
+    props.handleFlatTypeChange(event.target.value);
+  };
+
+  return (
+    <button className="dropdown-button" onClick={toggleDropdown}>
+      <img src="../../img/dropdownlogo.png" alt="dropdown icon" />
+      {isDropdownOpen && (
+        <DropdownMenu selectedValue={selectedValue} handleChange={handleChange} />
+      )}
+    </button>
+  );
+}
+
+type DropdownMenuProps = {
+  selectedValue: string;
+  handleChange: (event: ChangeEvent<HTMLSelectElement>) => void;
+};
+
+function DropdownMenu({ selectedValue, handleChange }: DropdownMenuProps) {
+  return (
+    <div className="dropdown-menu">
+      <label htmlFor="dropdown"></label>
+      <select id="dropdown" value={selectedValue} onChange={handleChange}>
+        <option value="">--Please choose an option--</option>
+        <option value="3 ROOM">3 room flats</option>
+        <option value="4 ROOM">4 room flats</option>
+        <option value="5 ROOM">5 room flats</option>
+      </select>
+    </div>
+  );
+}
+
+export default DropdownButton;
+
