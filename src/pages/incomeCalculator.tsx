6c0a79884da08910af86ab0c268b352e46666397
Jeff Tan <77041483+Arcyonix@users.noreply.github.com>
2023-03-12 22:03:08 +0800

    Add files via upload

--- src/pages/incomeCalculator.tsx
+++ src/pages/incomeCalculator.tsx
@@ -1,6 +1,5 @@
 import { PageProps } from '../functions/types';
 import '../css/incomeCal.css'
-import {calculate} from '../functions/incomecal';
 
 
 const IncomeCalculator = (props: PageProps) => {
@@ -20,27 +19,50 @@ const IncomeCalculator = (props: PageProps) => {
       <div className = 'seperator'>
       <div className="Inputs">
           <div>Cost Of Flat:</div>
+          <form>
+          <input type= "number" name="COF" placeholder='cost of flat'  />
+          </form>
+
      
    
       </div>
       <div className="Inputs">
           <div>Installment Period:</div>
+
+          <form>
+          <select name="INS" placeholder='10,15,20 Years'>
+          <option value="">Select Installment Period:</option>
+          <option value="10">10 years</option>
+          <option value="15">15 years</option>
+          <option value="20">20 years</option>
+          <option value="25">25 years</option>
+
+          </select>
+
+          </form>
+
      
    
       </div>
       <div className="Inputs">
           <div>Monthly Income:</div>
-     
+          <form>
+          <input name="INC" placeholder ='monthly income' type= "number" />
+          </form>
+
    
       </div>
       <div className="Inputs">
           <div>Down Payment:</div>
-     
+          <form>
+          <input name="PAY" placeholder= 'down payment' type= "number"/>
+          </form>
+
       </div>
 
       </div>
       <div className ="button">
-      <button id = "calculator" type ="button" onClick = {calculate}>Calculate</button>
+      <button id = "calculator" type ="button"  >Calculate</button>
       </div>
       <div className = "result"></div>
 
