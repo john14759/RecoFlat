ea112fc4993ea47474024df8a898f2ec2a92e94a
Vernonlzy <vernonlzy@gmail.com>
2023-03-29 17:38:19 +0800

    Edited LoanReco Css and function

--- src/pages/LoanRecommendation.tsx
+++ src/pages/LoanRecommendation.tsx
@@ -39,12 +39,11 @@ const LoanRecommendation = (props: PageProps) => {
       // let year = Year;
       let year = parseInt(Year.year);
       let month = parseInt(Month.month);
-    
+      var interest = principle * monthlyInterestRate;
+      var principlePaid = (monthlyPayment - interest);
+      principle = principle - principlePaid;
+      
       while (principle > 0) {
-        const interest = principle * monthlyInterestRate;
-        const principlePaid = (monthlyPayment - interest);
-        principle = principle - principlePaid;
-    
         const item: RepaymentScheduleItem = {
           date: `${month.toString().padStart(2, '0')}/${year}`,
           interestRate: interestRate,
@@ -54,14 +53,34 @@ const LoanRecommendation = (props: PageProps) => {
         };
         repaymentSchedule.push(item);
         
-        if (month === 12){
+        if (month == 12){
           year++;
           month=1;
         }
         else{
           month++;
         } 
+        interest = principle * monthlyInterestRate;
+        principlePaid = (monthlyPayment - interest);
+        principle = principle - principlePaid;
+      }
+      if(principle > -0.009){
+        const item: RepaymentScheduleItem = {
+          date: `${month.toString().padStart(2, '0')}/${year}`,
+          interestRate: interestRate,
+          monthlyInstalment: monthlyPayment,
+          interestPaid: interest,
+          endingPrinciple: principle
+        };
+        repaymentSchedule.push(item);
         
+        if (month == 12){
+          year++;
+          month=1;
+        }
+        else{
+          month++;
+        } 
       }
       
       setRepaymentSchedule(repaymentSchedule);
