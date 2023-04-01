65d720a0d40ec0754f40aeed6e120919dd686ade
Joe <jeremy.ongJW@gmail.com>
2023-04-02 01:13:37 +0800

    Setup google maps

--- src/pages/nearby.tsx
+++ src/pages/nearby.tsx
@@ -1,21 +1,22 @@
 import "../css/nearby.css"
 
 const Nearby = () => {
+  const apiKey = process.env.REACT_APP_GOOGLE_MAP_API_KEY
   return (
     <div className="nearby-container">
       Insert Map Here
-    </div>
-  )
-}
-/*
+
       <iframe
+        title="nearby-location"
         width="600"
         height="450"
         loading="lazy"
         allowFullScreen
         referrerPolicy="no-referrer-when-downgrade"
-        src="https://www.google.com/maps/embed/v1/place?key=
-          &q=Space+Needle,Seattle+WA">
+        src={"https://www.google.com/maps/embed/v1/place?key=" + apiKey + "&q=Space+Needle,Seattle+WA"}>
       </iframe>
-*/
+    </div>
+  )
+}
+
 export {Nearby}
